b3f7d1d962122c3192eea58487f9db8b7309a25e
hungryhao <hungryhao@163.com>
2015-03-16 12:16:38 +0800

    insertSort

--- src/com/hungry/sort/StraightInsertSort.java
+++ src/com/hungry/sort/StraightInsertSort.java
@@ -15,10 +15,21 @@ public class StraightInsertSort {
 		}
 	}
 
+	public void insertSort2(int[] num) {
+		int j;
+		for (int i = 1; i < num.length; i++) {
+			int k = num[i];
+			for (j = i; j > 0 && k < num[j - 1]; j--) {
+				num[j] = num[j - 1];
+			}
+			num[j] = k;
+		}
+	}
+
 	public static void main(String[] args) {
 		StraightInsertSort sis = new StraightInsertSort();
 		int[] num = { 2, 5, 6, 3, 7, 8 };
-		sis.insertSort(num);
+		sis.insertSort2(num);
 		for (int i = 0; i < num.length; i++) {
 			System.out.println(num[i]);
 		}
